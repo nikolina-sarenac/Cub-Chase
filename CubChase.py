aa28ebdbd69ed419b031b9125bca77378d200f6b
jovana-cerovac <jovanacerovac@gmail.com>
2019-11-27 16:47:47 +0100

    Prebacivanje na novi prozor klikom na Sart dugme

--- CubChase.py
+++ CubChase.py
@@ -20,20 +20,36 @@ class CubChase(QWidget):
         self.lbl = QLabel(self)
         self.lbl.setPixmap(self.pixmap)
 
-
         self.hbox.addWidget(self.lbl)
         self.setLayout(self.hbox)
 
         self.move(150, 100)
         self.setWindowTitle('Start window')
-        
 
         self.btn = QPushButton('Start', self)
+        self.btn.clicked.connect(self.showDialog)
         self.btn.resize(90, 50)
         self.btn.move(150, 340)
 
+        self.btn1 = QPushButton('', self)
+        self.btn1.setVisible(False)
+        self.show()
+
+    def showDialog(self):
+        self.pixmap = QPixmap("rsz_lion.jpg")
+        self.lbl.setPixmap(self.pixmap)
+
+        self.btn.setText('Single player')
+        self.btn.resize(150, 50)
+        self.btn.move(100, 400)
+
+        self.btn1.setText('Multiplayer')
+        self.btn1.setVisible(True)
+        self.btn1.resize(150, 50)
+        self.btn1.move(420, 400)
 
         self.show()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

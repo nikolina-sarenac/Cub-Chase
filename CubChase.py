a18552893e14323f974933438f0f0e3a5224632b
jovana-cerovac <jovanacerovac@gmail.com>
2019-12-04 21:08:24 +0100

    Lavirint

--- CubChase.py
+++ CubChase.py
@@ -4,14 +4,55 @@ from PyQt5.QtCore import (Qt, QSize)
 from PyQt5.QtGui import (QPixmap, QIcon, QCursor)
 from PyQt5 import QtCore
 import sys
-
 from PyQt5.uic.properties import QtCore
+from pygame.locals import *
+import pygame
+
+
+class Maze:
+    def __init__(self):
+        self.M = 22
+        self.N = 16
+        self.maze = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+    def draw(self, display_surf, image_surf):
+        bx = 0
+        by = 0
+        for i in range(0, self.M * self.N):
+            if self.maze[bx + (by * self.M)] == 1:
+                display_surf.blit(image_surf, (bx * 29, by * 30))
+
+            bx = bx + 1
+            if bx > self.M - 1:
+                bx = 0
+                by = by + 1
 
 
 class CubChase(QWidget):
+    windowWidth = 640
+    windowHeight = 480
 
     def __init__(self):
         super().__init__()
+        self.maze = Maze()
+        self._block_surf = None
+        self._display_surf = None
+        self._background = None
 
         self.initUI()
 
@@ -28,17 +69,15 @@ class CubChase(QWidget):
         self.setLayout(self.hbox)
 
         self.move(150, 100)
-        self.setWindowTitle('Start window')
+        self.setWindowTitle('Cub Chase')
 
         self.btn = QPushButton('', self)
         self.btn.clicked.connect(self.showDialog)
         self.btn.setStyleSheet('background-color: rgba(255, 255, 255, 0)')
-
         self.btn.setCursor(Qt.PointingHandCursor)
         self.btn.setIconSize(QSize(89, 46))
         self.btn.resize(100, 50)
         self.btn.move(120, 323)
-
 
         self.show()
 
@@ -53,10 +92,25 @@ class CubChase(QWidget):
         self.btn.setIconSize(QSize(89, 46))
         self.btn.resize(93, 50)
         self.btn.move(285, 355)
-        
-
+        self.btn.clicked.connect(self.showMaze)
 
         self.show()
+
+    def showMaze(self):
+        pygame.init()
+        pygame.display.set_caption('Cub Chase')
+        self.move(150, 100)
+        self._display_surf = pygame.display.set_mode((self.windowWidth, self.windowHeight), pygame.HWSURFACE)
+        self._block_surf = pygame.image.load("block3.jpg").convert()
+        self._background = pygame.image.load("playWin.png").convert()
+        self.on_render()
+        self.show()
+
+    def on_render(self):
+        screen = pygame.display.set_mode((self.windowWidth, self.windowHeight))
+        screen.blit(self._background, [0, 0])
+        self.maze.draw(self._display_surf, self._block_surf)
+        pygame.display.flip()
 
 
 if __name__ == '__main__':

09b396ad2fa1135ed63924fdf21300645d014b5c
nikolina-sarenac <nina.sarenac@hotmail.com>
2019-12-04 20:12:54 +0100

    Izmena pocetnog ekrana

--- CubChase.py
+++ CubChase.py
@@ -1,8 +1,11 @@
 from PyQt5.QtWidgets import (QWidget, QHBoxLayout,
                              QLabel, QApplication, QPushButton, QLineEdit, QInputDialog)
 from PyQt5.QtCore import (Qt, QSize)
-from PyQt5.QtGui import (QPixmap, QIcon)
+from PyQt5.QtGui import (QPixmap, QIcon, QCursor)
+from PyQt5 import QtCore
 import sys
+
+from PyQt5.uic.properties import QtCore
 
 
 class CubChase(QWidget):
@@ -14,7 +17,7 @@ class CubChase(QWidget):
 
     def initUI(self):
         self.hbox = QHBoxLayout(self)
-        self.pixmap = QPixmap("rsz_lion.jpg")
+        self.pixmap = QPixmap("pocetna.jpg")
         self.setMaximumSize(640, 480)
         self.setMinimumSize(640, 480)
 
@@ -29,11 +32,12 @@ class CubChase(QWidget):
 
         self.btn = QPushButton('', self)
         self.btn.clicked.connect(self.showDialog)
-        self.btn.setIcon(QIcon("startdugme.jpg"))
-        self.btn.setStyleSheet('background-color: #fdc086; border: 5px solid beige; font: Bold')
+        self.btn.setStyleSheet('background-color: rgba(255, 255, 255, 0)')
+
+        self.btn.setCursor(Qt.PointingHandCursor)
         self.btn.setIconSize(QSize(89, 46))
-        self.btn.resize(93, 50)
-        self.btn.move(150, 340)
+        self.btn.resize(100, 50)
+        self.btn.move(120, 323)
 
 
         self.show()
